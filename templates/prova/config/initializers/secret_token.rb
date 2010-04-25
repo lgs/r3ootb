2839ef32239d7f0eaba4e3f4f0d8b08a2140496e
Luca G. Soave <luca.soave@gmail.com>
2010-04-25 14:26:19 +0200

    fix path to templates

--- templates/prova/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = 'e91f175adea612e961e3c3cec6b74a0acdfe68b5e09a11c633b625f226f1088b2cf15092773493153ff4b5d56f48045eac82e285682666f178e6e46704f05c43'
