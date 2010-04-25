4134bb425d5f7ab0258979a766b95817f638dc09
Luca G. Soave <luca.soave@gmail.com>
2010-04-25 20:28:39 +0200

    add Out Of The Box Application Example

---
+++ app-example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '1a157a3d7af1cd1f9b92cc14c720e748cb8a7897e4738c477d8f6ea37edef5a9b6f33713362a9bec800420790e7944cb1cc9e0b1b6a892f16721df0e8469822c'
