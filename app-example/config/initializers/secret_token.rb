ffc42f472d78cf76b82bc729487ff5b891fe84d9
Luca G. Soave <luca.soave@gmail.com>
2010-04-26 23:52:47 +0200

    try to fix syntax error

--- app-example/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = 'f7eb14d2af6a378b3c26e92a0c3ab665d6828c969d8d6d1231554d2f50ddc0bd4fc484f7cae4b2eba4151e282f17f4a2b4464c5dddf093ad904e183d2e92efdb'
