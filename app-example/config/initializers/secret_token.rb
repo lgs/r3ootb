0adb5824ab1cc06104b7d9ab8188f865eb5a2c06
Luca G. Soave <luca.soave@gmail.com>
2010-06-14 23:44:52 +0200

    Rails 3.0.0.beta4: fix README.rdoc and sample app

--- app-example/config/initializers/secret_token.rb
+++ app-example/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = 'a29a21976753dc4abfdca4c4b00f889aad332dfcb58accbb4672ec28ad62232586b73c67c9e78cf134d2d8f2fd45f1fb1145a74d417963efdb61233f570dd17f'
+Rails.application.config.secret_token = 'd1be15d071cb9698df7a36b12ae2f9c581caf637db1ffa258bf4275f5a9bb99cc27820eaf4d1454a2672f3258230658aa21a03ebfdcade90697ae0a35002ff6a'
