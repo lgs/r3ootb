ff488534e703f59877215398e27d9bf5de2c5d14
Luca G. Soave <luca.soave@gmail.com>
2010-06-16 23:50:36 +0200

    try to fix stack:migrate bamboo

--- app-example/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = 'd1be15d071cb9698df7a36b12ae2f9c581caf637db1ffa258bf4275f5a9bb99cc27820eaf4d1454a2672f3258230658aa21a03ebfdcade90697ae0a35002ff6a'
